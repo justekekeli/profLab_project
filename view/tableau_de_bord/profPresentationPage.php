82d9a605be9996971feab70685df538edfbeb9b0
justekekeli <kekeliafonouvi@gmail.com>
2021-01-22 21:08:47 +0100

    Update with add of data on interfaces V10

--- view/tableau_de_bord/profPresentationPage.php
+++ view/tableau_de_bord/profPresentationPage.php
@@ -52,7 +52,7 @@
                  <?php
                     if($_SESSION['email']!=$theUser[0]['email']){
                       echo' <p>
-                      <button class="btn btn-theme02">Donner un avis</button>
+                      <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Donner un avis</button>
                        </p>';
                     }
                  ?>
@@ -106,6 +106,32 @@
             <!-- /col-lg-12 -->
           </div>
           <!-- /row -->
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Avis</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                  <form role="form" class="form-horizontal" action="index.php?action=avis&amp;prof=<?php echo $theUser[0]['email'];?>&amp;ref=<?php echo $_SESSION['email'];?>" method="post">
+                       <div class="form-group">
+                            <div class="col-lg-10">
+                            <textarea rows="4" cols="10" class="form-control" id="" name="avis" required></textarea>
+                            </div>
+                        </div>
+                        <button class="btn btn-theme" type="submit">valider</button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <button class="btn btn-theme04" type="button">Annuler</button>
+                              
+                    </form>
+              </div>
+
+            </div>
+          </div>
         </div>
         <!-- /container -->
       </section>
